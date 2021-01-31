8d181032f9c715ca75a39fb8da80b2689fa0108f
Gecaj <gecaj24@gmail.com>
2021-01-31 20:50:31 +0100

    Add validation.

--- src/main/java/com/smartbear/fileupload/dto/FileDto.java
+++ src/main/java/com/smartbear/fileupload/dto/FileDto.java
@@ -1,11 +1,16 @@
 package com.smartbear.fileupload.dto;
 
 import lombok.Data;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @Data
+@Validated
 public class FileDto {
+    @NonNull
     private final String name;
+    @NonNull
     private final List<String> tags;
 }
--- src/main/java/com/smartbear/fileupload/facade/FileFacade.java
+++ src/main/java/com/smartbear/fileupload/facade/FileFacade.java
@@ -4,8 +4,11 @@ import com.smartbear.fileupload.dto.FileDto;
 import com.smartbear.fileupload.model.File;
 import com.smartbear.fileupload.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -16,11 +19,19 @@ public class FileFacade {
     private final FileRepository fileRepository;
 
     public UUID addFile(FileDto fileDto) {
+        validateTags(fileDto.getTags());
         File file = fileRepository.save(File.builder()
                 .id(UUID.randomUUID())
                 .name(fileDto.getName())
                 .tags(tagsConverter.convertToString(fileDto.getTags()))
                 .build());
         return file.getId();
+    }
+
+    private void validateTags(List<String> tags) {
+        boolean containsInvalidChars = tags.stream().anyMatch(s -> s.contains("+") || s.contains("-") || s.contains(" "));
+        if (containsInvalidChars) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tags cannot contain '+', '-' and whitespace characters.");
+        }
     }
 }
